07a3c9fdbd02c934093e8c77100e64da8f482e34
Mike V <mike@iglab.dev>
2023-08-09 09:45:48 -0400

    move add template form to page and refac. style ob

--- frontend/src/components/Templates.jsx
+++ frontend/src/components/Templates.jsx
@@ -1,40 +1,34 @@
 import { useState, useEffect } from 'react'
-import { copyContent } from '../utilities/utilities'
 import templateService from '../services/templateService'
 import { useNavigate } from 'react-router-dom'
 
-import { HiPencilAlt, HiClipboardCopy, HiClipboardCheck, HiOutlineClipboardCopy } from 'react-icons/hi'
+import { HiPencilAlt, HiOutlineTrash } from 'react-icons/hi'
 
+import AddTemplateForm from './AddTemplateForm'
+import ConfirmModal from './shared/ConfirmModal'
 
-const styles = {
-  table: 'w-full rounded-lg',
-  th: 'bg-gray-300 border text-left px-2 py-3',
-  td: 'border px-2 py-1',
-  trBody: 'hover:bg-gray-200 cursor-pointer',
-  icons: 'text-xl text-gray-600 hover:text-teal-600 cursor-pointer'
-}
-
+import Styles from './Styles/Styles'
 
 const Templates = () => {
   
   const navigate = useNavigate()
 
   const [templates, settemplates] = useState([])
+  const [showAddForm, setShowAddForm] = useState(false)
+  const [updateTemplates, setUpdateTemplates] = useState(0)
 
-  const newSite = () => {
-    console.log('new site')
-    navigate('/addsite')
-  }
-
+  // Delete confirmation modal
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false)
+  
 
   /* Load templates */
   useEffect(() => {
     const fetchtemplates = async () => {
-      const templates = await templateservice.gettemplates()
+      const templates = await templateService.getTemplates()
       if(templates) settemplates(templates)
     }
     fetchtemplates()
-  }, [])
+  }, [updateTemplates])
 
 
   if (!templates) return (
@@ -44,36 +38,64 @@ const Templates = () => {
     </section>
   )
 
-  const handleRowClick = (id) => {
+  const handleEditClick = (id) => {
     console.log('row clicked', id)
-    navigate(`/site/${id}`)  
   }
+
+  const deleteTemplate = async (id) => {
+    const response = await templateService.deleteTemplate(id)
+    if (response) setUpdateTemplates(updateTemplates + 1)
+    else console.log('error deleting template')
+  }
+
+  const modalOptions = {
+    show: setDeleteModalOpen,
+    title: 'Delete Template',
+    message: 'Are you sure you want to delete this template?',
+    confirmLabel: 'Yes',
+    cancelLabel: 'No',
+    confirmationAction: deleteTemplate,
+    id: null
+  }
+
+  const handleDeleteClick = (template) => {
+    console.log(template.name)
+    if (!template.id) {
+      return console.log('error deleting template')
+    }
+    modalOptions.id = template.id
+    modalOptions.title = `${modalOptions.title} "${template.name}"`
+    setDeleteModalOpen(true)
+    console.log(deleteModalOpen)
+  }
+
+ 
 
   return (
     <section className='w-full pt-7'>
+      { deleteModalOpen && <ConfirmModal modalOptions={modalOptions} /> }
+      {showAddForm && <AddTemplateForm setShowAddForm={setShowAddForm} setUpdateTemplates={setUpdateTemplates} />}
       <header className='flex flex-row justify-between'>
-        <h1>Site List: </h1>
-        <button className='shadow bg-teal-600 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-3 rounded' onClick={newSite}>Add Site</button>
+        <h1>DNS Templates: </h1>
+        <button className='shadow bg-teal-600 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-3 rounded' onClick={() => setShowAddForm(true)}>Create New Template</button>
       </header>
       <section className='flex flex-row justify-between py-6'>
-        <table className={styles.table}>
+        <table className={Styles.table}>
           <thead>
             <tr>
-              <th className={styles.th}>Name</th>
-              <th className={styles.th}>Domain</th>
-              <th className={styles.th}></th>
+              <th className={Styles.th}>Name</th>
+              <th className={Styles.th}></th>
             </tr>
           </thead>
           <tbody>
-        {templates.map(site => {
+        {templates.map(template => {
           return (
-            <tr className={styles.trBody} key={site.id}>
-              <td className={styles.td}>{site.name}</td>
-              <td className={styles.td}>{site.domain}</td>
-              <td className={styles.td}>
+            <tr className={Styles.trBody} key={template.id}>
+              <td className={Styles.td} onClick={() => handleEditClick(template.id)}>{template.name}</td>
+              <td className={Styles.td}>
                 <div className='flex gap-2 flex-row justify-end'>
-                  <HiClipboardCopy className={styles.icons} onClick={() => copyContent('putcopydetailshere')}>Copy Link</HiClipboardCopy>
-                  <HiPencilAlt className={styles.icons} onClick={() => copyContent('putcopydetailshere')}>Edit</HiPencilAlt>
+                  <HiPencilAlt className={Styles.icons} onClick={() => handleEditClick(template.id)}>Edit</HiPencilAlt>
+                  <HiOutlineTrash className={Styles.icons} onClick={() => handleDeleteClick(template)}>Delete</HiOutlineTrash>
                 </div>
               </td>
             </tr>
