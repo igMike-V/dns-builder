9f98001dd89a125c2daa8477d801819243398400
Mike V <mike@iglab.dev>
2023-09-04 09:55:12 -0400

    fix  width of login fields

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -29,39 +29,39 @@ export const LoginForm = () => {
 
 
   return (
-    <div className='bg-gray-100 mb-8 p-4 rounded-lg max-w-4xl' >
-      <div className='flex align-middle gap-2 items-center mb-4'>
+    <div className='p-4 mb-8 bg-gray-100 rounded-lg' >
+      <div className='flex items-center gap-2 mb-4 align-middle'>
         <h1 className="font-bold">Login.</h1>
       </div>
       <form className='w-full pt-7'>
-        <div className="md:items-center mb-6">
+        <div className="mb-6 md:items-center">
           <div className="">
-            <label className="block text-gray-500 font-bold mb-1 md:mb-0 pr-4" >
+            <label className="block pr-4 mb-1 font-bold text-gray-500 md:mb-0" >
               Email Address
             </label>
           </div>
           <div className="" >
             <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500"
+              className="w-full px-4 py-2 leading-tight text-gray-700 bg-gray-200 border-2 border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-teal-500"
                 type="text" name="email" value={inputs.email} onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
             />
           </div>
         </div>
-        <div className="md:items-center mb-6">
+        <div className="mb-6 md:items-center">
           <div className="">
-            <label className="block text-gray-500 font-bold mb-1 md:mb-0 pr-4" >
+            <label className="block pr-4 mb-1 font-bold text-gray-500 md:mb-0" >
               Password
             </label>
           </div>
           <div className="" >
             <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500"
+              className="w-full px-4 py-2 leading-tight text-gray-700 bg-gray-200 border-2 border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-teal-500"
                 type="password" name="password" value={inputs.password} onChange={(e) => setInputs({...inputs, password: e.target.value}) }
             />
           </div>
         </div>
         <div className='flex gap-2'>
-          <button className="shadow bg-teal-600 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" onClick={(e) => handleSubmit(e)} >Login</button>
+          <button className="px-4 py-2 font-bold text-white bg-teal-600 rounded shadow hover:bg-teal-400 focus:shadow-outline focus:outline-none" onClick={(e) => handleSubmit(e)} >Login</button>
         </div>
         
       </form>
